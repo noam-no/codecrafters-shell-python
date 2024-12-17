ff2eab0d68449ec7bfa8627182efbe3a4696cda4
noam-no <noam.niort@etu.umontpellier.fr>
2024-12-17 19:13:01 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,14 +6,23 @@ commands = {}
 def invalid_command(input):
     if input not in commands:
         print(f"{input}: command not found")
+        
 
 def main():
-    sys.stdout.write("$ ")
+    try:
+        while True:
+            sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    invalid_command(command)
-
+            # Wait for user input
+            command = input()
+            invalid_command(command)
+    
+    
+    
+    
+    
+    except KeyboardInterrupt:
+        print("\nExiting shell ...")
 
 if __name__ == "__main__":
     main()
