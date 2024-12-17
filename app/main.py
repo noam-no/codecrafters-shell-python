b48b0e72327333d8afc6a52129084057ddcfedf6
noam-no <noam.niort@etu.umontpellier.fr>
2024-12-17 18:57:47 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
 import sys
 
+commands = {}
+
+
+def invalid_command(input):
+    if input not in commands:
+        print(f"{input}: command not found")
 
 def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
+    command = input()
+    invalid_command(command)
 
 
 if __name__ == "__main__":
