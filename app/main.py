a208194bc04999ab074542f7a045ef33e67b90e7
noam-no <noam.niort@etu.umontpellier.fr>
2024-12-17 20:23:40 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,28 +1,52 @@
-import sys
+#!/usr/bin/env python3
 
-commands = {}
+import sys, re, signal
+
+def signal_handler(sig, frame):
+    print("")
+    sys.stdout.write("$ ")
+    sys.stdout.flush()
+
+def exit_command(args):
+    print("exit")
+    if len(args) == 0:
+        pass
+    
+    elif re.match(r"\D",args[0]) != None:
+        print(f"bash: exit: {args[0]}: numeric argument required")
+    
+    elif len(args) > 1:
+        print("bash: exit: too many arguments")
+        return
+    
+    sys.exit(0)
 
 
-def invalid_command(input):
-    if input not in commands:
-        print(f"{input}: command not found")
-        
+commands = {"exit": exit_command}
+
+def command_parser(raw_input):
+    command = raw_input.split()
+    if not command:
+        return
+    header = command[0]
+    if header not in commands:
+        print(f"{header}: command not found")
+    else:
+        commands[header](command[1:])
 
 def main():
-    try:
-        while True:
+    while True:
+        try:
+            signal.signal(signal.SIGINT, signal_handler)
             sys.stdout.write("$ ")
+            sys.stdout.flush()
 
             # Wait for user input
             command = input()
-            invalid_command(command)
-    
-    
-    
-    
-    
-    except KeyboardInterrupt:
-        print("\nExiting shell ...")
+            command_parser(command)
+        except EOFError:
+            print("")
+            exit_command(["1"])
 
 if __name__ == "__main__":
     main()
